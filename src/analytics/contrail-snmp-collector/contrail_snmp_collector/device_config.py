2d44be805477013a9d74978e7c0518d0ee0ed6b5
Ted Ghose <rintu@hotmail.com>
2014-10-09 16:37:00 -0700

    mem-leak isolated + FlowExportSourceIp support

--- src/analytics/contrail-snmp-collector/contrail_snmp_collector/device_config.py
+++ src/analytics/contrail-snmp-collector/contrail_snmp_collector/device_config.py
@@ -2,10 +2,18 @@ from snmp import SnmpSession
 import copy
 
 class DeviceConfig(object):
-    def __init__(self, name, cfg={}, mibs=[]):
+    def __init__(self, name, cfg={}, mibs=[], flow_export_source_ip=None):
         self._raw = cfg
         self.name = name
         self.mibs = mibs or SnmpSession.TABLES() #all
+        self.flow_export_source_ip = flow_export_source_ip
+        self.snmp_name = None
+
+    def set_snmp_name(self, name):
+        self.snmp_name = name
+
+    def get_snmp_name(self):
+        return self.snmp_name
 
     def set_mibs(self, mib):
         if mib in SnmpSession.TABLES() and mib not in self.mibs:
@@ -13,6 +21,9 @@ class DeviceConfig(object):
 
     def get_mibs(self):
         return self.mibs
+
+    def get_flow_export_source_ip(self):
+        return self.flow_export_source_ip
         
     def snmp_cfg(self):
         cfg = copy.copy(self._raw)
